c9afe58d388890407860102c222021178aaa6217
Justin Collantes <justin@fasttrakcloud.com>
2018-09-11 22:02:34 -0400

    Updates

--- src/components/BaseComponent.js
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-
-export class BaseComponent extends Component {
-}
-
-export default BaseComponent;
